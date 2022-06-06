e7c33662322d5c1d20c2da8c4c61e8e7656227f6
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-06-06 12:06:07 -0700

    done

---
+++ utils/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+export const fetchApi = async (url) => {
+const {data} = await axios.get((url), {
+    headers: {
+        'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+        'X-RapidAPI-Key': 'd28414e1a2msh8f25ddd6d890f2cp114b41jsn96066f67d514'
+      },
+});
+
+return data;
+}
